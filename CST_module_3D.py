b3f36fe96fb2023cfdebc4efdb8626d9a8b6f7bb
leal26 <leal26@tamu.edu>
2018-05-22 16:09:02 -0600

    Formatted output of 3D CST function

--- CST_module_3D.py
+++ CST_module_3D.py
@@ -19,11 +19,13 @@ def interpolation_function(eta, shape = 'linear',
 def CST_3D(Bu, Bl, span, N={'eta':[0,1], 'N1':[.5, .5], 'N2':[1., 1.], 'chord':[1., 0]},
            mesh = (100,100), chord = {'eta':[0,1], 'A':[1.], 'N1':1, 'N2':1, 'initial':1., 'final':0.1}, 
            sweep = {'eta':[0,1], 'A':[1.], 'N1':1, 'N2':1, 'initial':0, 'final':0},
-           twist = {'eta':[0,1], 'A':[1.], 'N1':1, 'N2':1, 'initial':0, 'final':1}):
+           twist = {'eta':[0,1], 'A':[1.], 'N1':1, 'N2':1, 'initial':0, 'final':1},
+           surfaces = 'both'):
     """
     - Bu: upper shape coefficients
     - Bl: lower shape coefficients
     - mesh: list of number of points in x and y
+    - surfaces: surfaces to be analyzed (options: 'upper', 'lower', and 'both'.
     """
     def S(B, psi, eta):
         """ Cross section shape function. Validated for high dimensions.
@@ -89,8 +91,10 @@ def CST_3D(Bu, Bl, span, N={'eta':[0,1], 'N1':[.5, .5], 'N2':[1., 1.], 'chord':[
 
     for i in range(mesh[0]):
         for j in range(mesh[1]):
-            zeta_u[j][i] = C(psi[i], eta[j])*S(Bu, psi[i], eta[j])
-            zeta_l[j][i] = -C(psi[i], eta[j])*S(Bl, psi[i], eta[j])
+            if surfaces == 'upper' or surfaces == 'both':
+                zeta_u[j][i] = C(psi[i], eta[j])*S(Bu, psi[i], eta[j])
+            if surfaces == 'lower' or surfaces == 'both':
+                zeta_l[j][i] = -C(psi[i], eta[j])*S(Bl, psi[i], eta[j])
 
     chord_distribution = CST(eta, chord['eta'][1], chord['initial'], Au=chord['A'], N1=chord['N1'], N2=chord['N2'], deltasLE=chord['final'])
     sweep_distribution = CST(eta, sweep['eta'][1], deltasz = sweep['final'], Au=sweep['A'], N1=sweep['N1'], N2=sweep['N2'])
@@ -102,12 +106,6 @@ def CST_3D(Bu, Bl, span, N={'eta':[0,1], 'N1':[.5, .5], 'N2':[1., 1.], 'chord':[
     x = np.zeros(len(psi))
     for i in range(len(x)):
         x[i] = psi[i]*chord_distribution[i]
-    print 'chord'
-    print chord_distribution
-    print 'sweep'
-    print sweep_distribution
-    print 'twist'
-    print twist_distribution
     y = eta
 
     X = np.zeros(mesh)
@@ -128,22 +126,47 @@ def CST_3D(Bu, Bl, span, N={'eta':[0,1], 'N1':[.5, .5], 'N2':[1., 1.], 'chord':[
                 xr = twist['psi_root']
                 zr = twist['zeta_root']
                 R = Rotation(twist['axis'][0], twist['axis'][1], twist['axis'][2],
-                         twist_distribution[j])                
-                t_u = np.array([X[j][i]-xr,Y[j][i],Z_u[j][i]-zr])
-                t_l = np.array([X[j][i]-xr,Y[j][i],Z_l[j][i]-zr])
-                T_u = R.dot(t_u)
-                T_l = R.dot(t_l)
-                
-                X_u[j][i] = xr + T_u[0]
-                X_l[j][i] = xr + T_l[0]
-                Y_u[j][i] = T_u[1]
-                Y_l[j][i] = T_l[1]
-                Z_u[j][i] = zr + T_u[2]
-                Z_l[j][i] = zr + T_l[2]
-    if twist == None:
-        return [X,Y,Z_u,Z_l]
+                         twist_distribution[j])
+                if surfaces == 'upper' or surfaces == 'both':
+                    t_u = np.array([X[j][i]-xr,Y[j][i],Z_u[j][i]-zr])
+                    T_u = R.dot(t_u)
+                    X_u[j][i] = xr + T_u[0]
+                    Y_u[j][i] = T_u[1]
+                    Z_u[j][i] = zr + T_u[2]
+                    
+                if surfaces == 'lower' or surfaces == 'both':
+                    t_l = np.array([X[j][i]-xr,Y[j][i],Z_l[j][i]-zr])
+                    T_l = R.dot(t_l)
+                    X_l[j][i] = xr + T_l[0]
+                    Y_l[j][i] = T_l[1]
+                    Z_l[j][i] = zr + T_l[2]
+    # Formating data for output
+    if surfaces == 'upper' or surfaces == 'both':
+        output_u = {}
+        if twist == None:
+            output_u['x'] = X
+            output_u['y'] = Y
+        else:
+            output_u['x'] = X_u
+            output_u['y'] = Y_u
+        output_u['z'] = Z_u
+
+    if surfaces == 'upper' or surfaces == 'both':
+        output_l = {}
+        if twist == None:
+            output_l['x'] = X
+            output_l['y'] = Y
+        else:
+            output_l['x'] = X_l
+            output_l['y'] = Y_l
+        output_l['z'] = Z_l
+    if surfaces == 'upper':
+        return output_u
+    elif surfaces == 'lower':
+        return output_l
     else:
-        return [X_u,X_l,Y_u,Y_l,Z_u,Z_l]
+        return {'upper':output_u,
+                'lower':output_l}
 
 if __name__ == '__main__':
     from mpl_toolkits.mplot3d import Axes3D
@@ -180,14 +203,16 @@ if __name__ == '__main__':
     sweep = {'eta':[0,1], 'A':[0.], 'N1':Nb, 'N2':Na, 
              'initial':initial_nosecone_x, 'final':final_nosecone_x}
     twist = {'eta':[0,1], 'A':[0.], 'N1':1, 'N2':1,'initial':0.5, 'final':0, 'psi_root':0.2,'zeta_root':0, 'axis':axis}    
-    [X_u,X_l,Y_u,Y_l,Z_u, Z_l] = CST_3D(B, B,  span, N, mesh, chord, sweep, twist)
+    
+    output = CST_3D(B, B,  span, N, mesh, chord, sweep, twist)
 
     fig = plt.figure()
     ax = fig.gca(projection='3d')
-    surf_u = ax.plot_surface(X_u, Z_u, Y_u, cmap=plt.get_cmap('jet'),
-                       linewidth=0, antialiased=False)
-    surf_l = ax.plot_surface(X_l, Z_l, Y_l, cmap=plt.get_cmap('jet'),
-                       linewidth=0, antialiased=False)
+    
+    for surface in output:
+        ax.plot_surface(output[surface]['x'], output[surface]['z'], 
+                        output[surface]['y'], cmap=plt.get_cmap('jet'),
+                        linewidth=0, antialiased=False)
     # cset = ax.contour(X, Z_u, Y, zdir='z', offset=0, cmap=cm.coolwarm)
     # cset = ax.contour(X, Z_l, Y, zdir='z', offset=0,  cmap=cm.coolwarm)
     # cset = ax.contour(X, Z_u, Y, zdir='x', offset=-.1, cmap=cm.coolwarm)
@@ -198,12 +223,15 @@ if __name__ == '__main__':
     # Customize the z axis.
     ax.set_zlim(0, 4)
 
-    max_range = np.array([X_u.max()-X_u.min(), X_l.max()-X_l.min(), Z_u.max()-Z_l.min(),
-                          Y_u.max()-Y_u.min(), Y_l.max()-Y_l.min()]).max() / 2.0
+    max_range = np.array([output['upper']['x'].max()-output['upper']['x'].min(),
+                          output['lower']['y'].max()-output['lower']['y'].min(),
+                          output['upper']['z'].max()-output['lower']['z'].min(),
+                          output['upper']['y'].max()-output['upper']['y'].min(),
+                          output['lower']['y'].max()-output['lower']['y'].min()]).max() / 2.0
 
-    mid_x = (X_u.max()+X_l.min()) * 0.5
-    mid_y = (Y_u.max()+Y_l.min()) * 0.5
-    mid_z = (Z_u.max()+Z_l.min()) * 0.5
+    mid_x = (output['upper']['x'].max()+output['lower']['x'].min()) * 0.5
+    mid_y = (output['upper']['y'].max()+output['lower']['x'].min()) * 0.5
+    mid_z = (output['upper']['z'].max()+output['lower']['z'].min()) * 0.5
     ax.set_xlim(mid_x - max_range, mid_x + max_range)
     ax.set_ylim(mid_z - max_range, mid_z + max_range)
     ax.set_zlim(mid_y - max_range, mid_y + max_range)
@@ -211,15 +239,15 @@ if __name__ == '__main__':
     plt.ylabel('z')
     plt.show()
 
-    fig = plt.figure()
-    ax = fig.gca(projection='3d')
+    # fig = plt.figure()
+    # ax = fig.gca(projection='3d')
 
-    ax.plot_trisurf(X_u.flatten(),  Y_u.flatten(), Z_u.flatten(), linewidth=0.2, antialiased=True,)
-    ax.plot_trisurf(X_l.flatten(),  Y_l.flatten(), Z_l.flatten(), linewidth=0.2, antialiased=True,)
-    ax.plot([sweep['initial'] + twist['psi_root']*chord['initial'],sweep['initial'] + twist['psi_root']*chord['initial'] + axis[0]],[0,axis[1]])
-    ax.set_xlim(mid_x - max_range, mid_x + max_range)
-    ax.set_ylim(mid_y - max_range, mid_y + max_range)
-    ax.set_zlim(mid_z - max_range, mid_z + max_range)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.show()
+    # ax.plot_trisurf(X_u.flatten(),  Y_u.flatten(), Z_u.flatten(), linewidth=0.2, antialiased=True,)
+    # ax.plot_trisurf(X_l.flatten(),  Y_l.flatten(), Z_l.flatten(), linewidth=0.2, antialiased=True,)
+    # ax.plot([sweep['initial'] + twist['psi_root']*chord['initial'],sweep['initial'] + twist['psi_root']*chord['initial'] + axis[0]],[0,axis[1]])
+    # ax.set_xlim(mid_x - max_range, mid_x + max_range)
+    # ax.set_ylim(mid_y - max_range, mid_y + max_range)
+    # ax.set_zlim(mid_z - max_range, mid_z + max_range)
+    # plt.xlabel('x')
+    # plt.ylabel('y')
+    # plt.show()
