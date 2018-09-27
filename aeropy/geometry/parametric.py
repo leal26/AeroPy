ab290a53f8f9a319be0f987094f93e8097488594
Pedro Leal <leal26@tamu.edu>
2018-09-26 21:13:21 -0500

    Added Bishop frame for 2D

--- aeropy/geometry/parametric.py
+++ aeropy/geometry/parametric.py
@@ -22,6 +22,9 @@ class poly():
         """ d^2z2 / dz1^2 (checked)"""
         return(6*self.a[0]*z1 + 2*self.a[1])
 
+    def inflection_points(self):
+        return([-self.a[1]/(3*self.a[0])])
+
     def x1z1(self, z1):
         """ dx1/ dz1 (checked)"""
         return(np.sqrt(1+(self.z2z1(z1))**2))
@@ -45,7 +48,7 @@ class poly():
 
     def z1x11(self, z1):
         """ d^2z1 / dz1^2 """
-        return(self.z1x1(z1)**3*self.z2z1(z1)*self.z2z11(z1))
+        return(-self.z1x1(z1)**4*self.z2z1(z1)*self.z2z11(z1))
 
     def x1(self, z_final):
         """ d^2z2 / dz1^2 (checked)"""
@@ -87,14 +90,25 @@ class poly():
         """ Position anywhere along shell considering shell thickness """
         return(self.neutral_line(z1) + x2*self.g2(z1))
 
-    def rx1(self, z1):
+    def rx1(self, z1, normalize=True):
         """ Position anywhere along shell considering shell thickness """
         output = np.array([self.z1x1(z1), self.z2x1(z1)])
+        if normalize:
+            output = output.T
+            for i in range(len(output)):
+                output[i] /= np.linalg.norm(output[i])
+            output = output.T
         return(output)
 
-    def rx11(self, z1):
+    def rx11(self, z1, normalize=True):
         """ Position anywhere along shell considering shell thickness """
-        return(np.array([self.z1x11(z1), self.z2x11(z1)]))
+        output = np.array([self.z1x11(z1), self.z2x11(z1)])
+        if normalize:
+            output = output.T
+            for i in range(len(output)):
+                output[i] /= np.linalg.norm(output[i])
+            output = output.T
+        return(output)
 
     def g11(self, z1, x2=0):
         g1 = self.g1(z1)
@@ -103,6 +117,105 @@ class poly():
     def g22(self, z1):
         g2 = self.g2(z1)
         return(np.inner(g2, g2))
+
+
+class frame():
+    def __init__(self, curve=poly(), frame='Frenet-Serret',
+                 z1=np.linspace(0, 1, 11)):
+        self.curve = poly()
+        self.frame = frame
+        self.z1 = z1
+        self.z2 = self.curve.z2(z1)
+
+    def z1_default(self, z1):
+        if z1 is None:
+            return(self.z1)
+        else:
+            return(z1)
+
+    def bishop(self, z1=None):
+        z1 = self.z1_default(z1)
+        T = self.T(z1)
+        alpha = self.alpha(z1)
+        # print('here')
+        # print(alpha)
+
+        M1 = self.M1(z1=z1, alpha=alpha)
+        M2 = self.M2(z1=z1, alpha=alpha)
+
+        return(T, M1, M2)
+
+    def frenet_serret(self, z1=None):
+        z1 = self.z1_default(z1)
+        T = self.T(z1)
+        N = self.N(z1=z1)
+        B = self.B(z1=z1)
+
+        return(T, N, B)
+
+    def T(self, z1=None):
+        z1 = self.z1_default(z1)
+        return(self.curve.rx1(z1))
+
+    def N(self, z1=None):
+        z1 = self.z1_default(z1)
+        return(self.curve.rx11(z1))
+
+    def M1(self, z1=None, alpha=None):
+        z1 = self.z1_default(z1)
+        if alpha is None:
+            alpha = self.alpha(z1)
+        if self.frame == 'Bishop':
+            # print(self.N(z1))
+            # print(self.B(z1))
+            print(np.cos(alpha))
+            print(np.sin(alpha))
+            new = np.cos(alpha)*self.N(z1) - np.sin(alpha)*self.B(z1)
+            print('new')
+            print(new)
+            print(self.N(z1))
+            return(np.cos(alpha)*self.N(z1) -
+                   np.sin(alpha)*self.B(z1))
+
+    def M2(self, z1=None, alpha=None):
+        z1 = self.z1_default(z1)
+        if alpha is None:
+            alpha = self.alpha(z1)
+        if self.frame == 'Bishop':
+            return(np.sin(alpha)*self.N(z1) +
+                   np.cos(alpha)*self.B(z1))
+
+    def B(self, z1=None):
+        z1 = self.z1_default(z1)
+        try:
+            return(np.ones(len(z1)))
+        except(ValueError):
+            return(1)
+
+    def curvature(self, z1=None):
+        z1 = self.z1_default(z1)
+        return(self.curve.rx11(z1))
+
+    def torsion(self, z1=None):
+        z1 = self.z1_default(z1)
+        try:
+            return(np.zeros(len(z1)))
+        except(ValueError):
+            return(0)
+
+    def alpha(self, z1=None, alpha0=0.0):
+        z1 = self.z1_default(z1)
+        alpha_list = []
+        inflections = self.curve.inflection_points()
+        j = 0
+        alpha_j = alpha0
+        for i in range(len(z1)):
+            if j != len(inflections):
+                if z1[i] >= inflections[j]:
+                    alpha_j += math.pi
+                    j += 1
+            alpha_list.append(alpha_j)
+        return(np.array(alpha_list))
 
 
 def B(x, k, i, t):
--- examples/structural_analysis.py
+++ examples/structural_analysis.py
@@ -1,26 +1,25 @@
-from aeropy.geometry.parametric import poly
+from aeropy.geometry.parametric import poly, frame
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 curve = poly()
-
-
-z1 = np.linspace(0, 1, 10)
+z1 = np.linspace(0, 1)
 z2 = curve.z2(z1)
-g1 = curve.g1(z1)
-g2 = curve.g2(z1)
-rx1 = curve.rx1(z1)
-rx11 = curve.rx11(z1)
+
+ccs = frame(curve=curve, frame='Bishop')
+[T, N, B] = ccs.frenet_serret()
+g1 = curve.g1(ccs.z1)
+g2 = curve.g2(ccs.z1)
 
 plt.figure()
 plt.plot(z1, z2, label='geometry')
-plt.quiver(z1, z2, g1[0], g1[1], angles='xy', label='CCS')
-plt.quiver(z1, z2, g2[0], g2[1], angles='xy')
-plt.quiver(z1, z2, rx1[0], rx1[1], angles='xy', color='g')
-plt.quiver(z1, z2, rx11[0], rx11[1], angles='xy', color='g')
+plt.quiver(ccs.z1, ccs.z2, g1[0], g1[1], angles='xy', label='Truth')
+plt.quiver(ccs.z1, ccs.z2, g2[0], g2[1], angles='xy')
+plt.quiver(ccs.z1, ccs.z2, T[0], T[1], angles='xy', color='g',
+           label='Bishop')
+plt.quiver(ccs.z1, ccs.z2, N[0], N[1], angles='xy', color='g')
 plt.legend()
 plt.axis('equal')
-plt.grid()
 plt.show()
