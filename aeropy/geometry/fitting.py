846409a8e7aca8f4146e46cfeac2febd89444c34
Pedro Leal <leal26@tamu.edu>
2018-11-28 19:20:50 -0600

    Recreating fitting package

--- aeropy/geometry/fitting.py
+++ aeropy/geometry/fitting.py
@@ -2,15 +2,112 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import math
-import stl
 from scipy.optimize import minimize, differential_evolution
 from scipy.spatial.distance import directed_hausdorff
-import pickle
-from numpy import linalg as LA
+from multiprocessing import Pool
+import time
 
 from aeropy.xfoil_module import output_reader
-from aeropy.geometry.wing import Rotation_euler_vector
-from aeropy.CST_3D.module import CST_3D, dimensionalize, ControlPoints
+
+
+class fitting():
+    def __init__(self, **params):
+        self.object = params.get('object', np.linspace(10, 50, 9))
+        self.update = params.get('update', np.linspace(10, 50, 9))
+        self.x0 = params.get('x0', np.linspace(10, 50, 9))
+        self.p1 = params.get('p1', np.linspace(10, 50, 9))
+        self.p2 = params.get('p2', np.linspace(10, 50, 9))
+        self.p1_name = params.get('p1_name', 'Parameter 1')
+        self.p2_name = params.get('p2_name', 'Parameter 2')
+        self.calculate_points = params.get('calculate_points', 0)
+        self.raw = params.get('raw', 0)
+        self.callback = params.get('callback', None)
+
+    def convergence_study(self, parallel=True):
+        P1_f, P2_f = self._format_parameters()
+        if parallel:
+            p = Pool(len(P1_f))
+        else:
+            p = Pool(1)
+
+        input = np.vstack([P1_f, P2_f]).T
+        self.solutions = p.map(self.find, input)
+        self.error = np.array([self.solutions[i]['fun'] for i in
+                               range(len(self.solutions))])
+        self.error = self.error.reshape(self.P1.shape)
+        self.rel_error = self.error/self.error[0][0]
+
+    def find(self, param_i):
+        '''
+        inputs: [location, XYZ, sy, ny, xshear]'''
+        p1_i, p2_i = param_i
+        x0 = self.x0(p1_i, p2_i)
+        start = time.time()
+        solution = minimize(self.shape_difference, x0, args=param_i)
+        end = time.time()
+        error = solution['fun']
+        print('p1=%i\t p2=%i\t error=%f\t time=%f' % (p1_i, p2_i, error,
+                                                      end-start))
+        if self.callback is not None:
+            self.callback(self.object, p1_i, p2_i)
+        return solution
+
+    def shape_difference(self, x, param_i):
+        p1_i, p2_i = param_i
+        self.update(self.object, x, p1_i, p2_i)
+        points = self.calculate_points(self.object, self.raw)
+
+        if self.raw.ndim == 3:
+            error = 0
+            N = 0
+            for i in range(len(points)):
+                error += np.linalg.norm(points[i] - self.raw[i])**2
+                N += len(points[i])
+        else:
+            error = np.linalg.norm(points[0] - self.raw)**2
+            N = len(points)
+        return(error/N)
+
+    def plot_study(self, relative=True):
+        if relative:
+            z = self.rel_error
+        else:
+            z = self.error
+        fig, ax = plt.subplots()
+        cs = ax.contourf(self.P1, self.P2, z, np.linspace(0, 1, 101))
+
+        fig.colorbar(cs, ticks=np.linspace(0, 1, 6))
+        # plt.clim(0, 1)
+        plt.xlabel(self.p1_name)
+        plt.ylabel(self.p2_name)
+        plt.show()
+
+    def plot_fit(self):
+        network = self.calculate_points(self.object, self.raw)
+        plt.figure()
+        if network.ndim == 3:
+            for i in range(len(network)):
+                plt.scatter(network[i, :, 0], network[i, :, 2], c='b',
+                            label='fit')
+                plt.scatter(self.raw[i, :, 0], self.raw[i, :, 2], c='r',
+                            label='raw')
+        else:
+            plt.scatter(network[:, 0], network[:, 2], c='b', label='fit')
+            plt.scatter(self.raw[:, 0], self.raw[:, 2], c='r', label='raw')
+        plt.legend()
+        plt.show()
+
+    def _format_parameters(self, array_type=int):
+        self.p1 = self.p1.astype(array_type)
+        self.p2 = self.p2.astype(array_type)
+        self.P1, self.P2 = np.meshgrid(self.p1, self.p2)
+        P1_f = self.P1.flatten()
+        P2_f = self.P2.flatten()
+        return(P1_f, P2_f)
+
+###############
+# Old stuff that will get deleted
+############
 
 
 def process_data(upper, lower, LE, TE):
--- examples/fitting_edges.py
+++ examples/fitting_edges.py
@@ -1,136 +1,110 @@
 import aeropy.CST_3D as cst
 from aeropy.filehandling.vtk import generate_surface
 import aeropy.CST_3D.mesh_tools as meshtools
+from aeropy.geometry.fitting import fitting
 
 from scipy.spatial.distance import directed_hausdorff
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pickle
 
 
-def convergence_study(Ny=np.linspace(10, 50, 9),
-                      Ns=np.linspace(10, 50, 9)):
-    Ny = Ny.astype(int)
-    Ns = Ns.astype(int)
-    NY, NS = np.meshgrid(Ny, Ns)
-    error = np.zeros(NY.shape)
-    for i in range(len(NY)):
-        for j in range(len(NY[0])):
-            error[i][j] = find_edges(NY[i][j], NS[i][j])
-            print('Ny=%i, Ns=%i, error=%f' % (i, j, error[i][j]))
-
-    f = open('convergence_edges.p', 'wb')
-    pickle.dump(error, f)
-
-    fig, ax = plt.subplots()
-    cs = ax.contourf(NY, NS, error)
-    fig.colorbar(cs)
-    plt.xlabel('Bernstein order for taper')
-    plt.ylabel('Bernstein order for shear')
-    plt.show()
-
-
-def find_edges(Ny, Ns):
+def x0(Ny, Ns):
     '''
     inputs: [location, XYZ, sy, ny, xshear]'''
 
     XZ = [20, 10]
     A_y = (Ny+1)*[1./(Ny+1)]
     A_xshear = (Ns+1)*[1./(Ns+1)]
-
     x0 = XZ + A_y + A_xshear
-
-    solution = minimize(shape_difference, x0, args=(Ny, Ns))
-    error = solution['fun']
-    return error
+    return x0
 
 
-def shape_difference(x, Ny, Ns):
-    fuselage.XYZ = [x[0], Y, x[1]]
-    m = 2 + Ny + 1
-    fuselage.sy = cst.BernsteinPolynomial(Ny, x[2:m])
-    n = m + Ns
-
-    fuselage.xshear = cst.BernsteinPolynomial(Ns, [0] + list(x[m: n]))
-
-    # Option trying to use norm
-    lower_e, upper_e = convert_points()
-
-    error = np.linalg.norm(lower_e - lower)
-    error += np.linalg.norm(upper_e - upper)
-    return(error)
+def update(fuselage, x, p1_i, p2_i):
+    fuselage.XYZ[0] = x[0]
+    fuselage.XYZ[2] = x[1]
+    m = 2 + p1_i + 1
+    fuselage.sy = cst.BernsteinPolynomial(p1_i, x[2: m])
+    n = m + p2_i
+    fuselage.xshear = cst.BernsteinPolynomial(p2_i, [0] + list(x[m: n]))
 
 
-def convert_points():
-    x_raw, y_raw, z_raw = upper.T
+def calculate_points(fuselage, raw):
+    x_raw, y_raw, z_raw = raw[0].T
     psi_u, eta_u = fuselage.inverse(x_raw, y_raw, z_raw)
-    x_raw, y_raw, z_raw = lower.T
+    x_raw, y_raw, z_raw = raw[1].T
     psi_l, eta_l = fuselage.inverse(x_raw, y_raw, z_raw)
-
     psi_e = np.vstack((np.zeros(len(eta_l)),
                        np.ones(len(eta_u))))
     eta_e = np.vstack((eta_l, eta_u))
     mesh_e = fuselage(psi_e, eta_e)
-    lower_e, upper_e = np.dstack(mesh_e)
-    return(lower_e, upper_e)
+    return(np.dstack(mesh_e))
 
 
-raw_data = pickle.load(open('fuselage.p', 'rb'))
+if __name__ == "__main__":
+    raw_data = pickle.load(open('fuselage.p', 'rb'))
 
-upper = pickle.load(open('upper.p', 'rb'))
-lower = pickle.load(open('lower.p', 'rb'))
-eta_cp = np.linspace(0, 1, 4)
-# fuselage parameters
+    upper = pickle.load(open('upper.p', 'rb'))
+    lower = pickle.load(open('lower.p', 'rb'))
+    raw = np.array([lower, upper])
+    eta_cp = np.linspace(0, 1, 4)
 
-# Order
-Ny = 30
-Ns = 20
+    # Order
+    Ny = 30
+    Ns = 20
 
-# Dummy start
-location = [0, 0., 0]
-Y = max([max(upper[:, 0]), max(lower[:, 0])])
+    # Dummy start
+    location = [0, 0., 0]
+    Y = max([max(upper[:, 0]), max(lower[:, 0])])
+    XZ = [20, 10]
+    XYZ = [XZ[0], Y, XZ[1]]
+    A_y = (Ny+1)*[0.15]
+    A_xshear = (Ns+1)*[0]
+    nx = [.5, .5]
+    ny = [0.75, 0]
 
-XZ = [20, 10]
-XYZ = [XZ[0], Y, XZ[1]]
-A_y = (Ny+1)*[0.15]
-A_xshear = (Ns+1)*[0]
-nx = [.5, .5]
-ny = [0.75, 0]
+    f_sy = cst.BernsteinPolynomial(Ny, A_y)
+    f_sx = cst.BernsteinPolynomial(1, [1, 1])
+    f_xshear = cst.BernsteinPolynomial(Ns, A_xshear)
 
-f_sy = cst.BernsteinPolynomial(Ny, A_y)
-f_sx = cst.BernsteinPolynomial(1, [1, 1])
-f_xshear = cst.BernsteinPolynomial(Ns, A_xshear)
+    fuselage = cst.CST3D(rotation=(0., -90., -90.),
+                         location=location,
+                         XYZ=XYZ,
+                         nx=nx,
+                         sy=f_sy,
+                         ref=(0.5, 0., 0.),
+                         ny=ny,
+                         xshear=f_xshear)
 
-fuselage = cst.CST3D(rotation=(0., -90., -90.),
-                     location=location,
-                     XYZ=XYZ,
-                     nx=nx,
-                     sy=f_sy,
-                     ref=(0.5, 0., 0.),
-                     ny=ny,
-                     xshear=f_xshear)
+    N_chord = 50
+    N_span = 100
 
-N_chord = 50
-N_span = 100
+    # find_edges(Ny, Ns)
+    # f = open('fuselage_object.p', 'wb')
+    # pickle.dump(fuselage, f)
 
-find_edges(Ny, Ns)
-f = open('fuselage_object.p', 'wb')
-pickle.dump(fuselage, f)
-network_e = convert_points()
-lower_e, upper_e = network_e
-# convergence_study(Ny=np.linspace(5, 25, 5),
-#                   Ns=np.linspace(5, 25, 5))
+    study = fitting(object=fuselage,
+                    update=update,
+                    p1=np.linspace(5, 30, 6),
+                    p2=np.linspace(5, 30, 6),
+                    p1_name='Berstein order for Sy',
+                    p2_name='Berstein order for shear',
+                    x0=x0,
+                    raw=raw,
+                    calculate_points=calculate_points)
+    study.convergence_study(parallel=True)
+    study.plot_study()
 
-plt.figure()
-plt.scatter(lower_e[:, 0], lower_e[:, 2], c='b', label='fit')
-plt.scatter(upper_e[:, 0], upper_e[:, 2], c='b')
-plt.scatter(lower[:, 0], lower[:, 2], c='r', label='raw')
-plt.scatter(upper[:, 0], upper[:, 2], c='r')
-plt.legend()
-plt.show()
+    # Store convergence data
+    f = open('convergence_edges.p', 'wb')
+    pickle.dump(study, f)
 
-# Generating vtk files
-network_e = np.array([lower_e, upper_e])
-generate_surface(network_e, "fuselage_edges")
+    # Plot it
+    study.plot_fit()
+
+    # Generating vtk files
+    network = calculate_points(fuselage, raw)
+    generate_surface(network, "fuselage_edges")
