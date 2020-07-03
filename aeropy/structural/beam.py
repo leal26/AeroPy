Fixed second derivative and large deflection working

aeropy/structural/beam.py
@@ -187,7 +187,9 @@ class beam_chen():
             rhs = self.g.rho[i] - self.g_p.rho[i]
             lhs = self.M[i]/self.p.young/self.p.inertia
             self.r[i] = np.abs(lhs - rhs)
-
+        print('M', self.M)
+        print('rho', self.g.rho)
+        print('Rho', self.g_p.rho)
         if self.ignore_ends:
             self.R = trapz(self.r[1:-1], self.s[1:-1])
         else:
@@ -244,7 +246,7 @@ class beam_chen():
         self.calculate_residual()
         return self.R
 
-    def integral_ends(self, eps=1e-4):
+    def integral_ends(self, eps=1e-7):
         # Correct point
         origin = np.array([0])
         tip = np.array([self.g.chord])

examples/structural/beam_CST/straight_distributed_follower.py
@@ -36,8 +36,8 @@ def format_input(input):
 g = CoordinateSystem.CST(D=[0, 0, 0, 0, 0], chord=1, color='b', N1=1, N2=1)
 p = properties()
 w = p.young*p.inertia*lamda/p.length**3
-s = np.linspace(0, 1, 10)
-print('w', w)
+s = np.linspace(0, 1, 11)
+
 plt.figure()
 g0 = CoordinateSystem.polynomial(D=[0, 0, 0, 0], chord=1, color='k')
 g0.calculate_x1(s)
@@ -45,23 +45,22 @@ g0.plot(label='Parent', zorder=0)
 
 l = loads(distributed_load=distributed_load, follower=False)
 b = beam_chen(g, p, l, s, ignore_ends=True)
-b.parameterized_solver(format_input=format_input, x0=[0.475708, 0.359857,
-                                                      0.250499, 0.176188])
+b.parameterized_solver(format_input=format_input, x0=g.D[:-1])
 print('x', b.x)
 print('y', b.y)
-print('chord', b.chord)
-print('deltaz', b.deltaz)
+print('chord', b.g.chord)
+print('deltaz', b.g.deltaz)
 plt.plot(b.x, b.y, '.3', label=r'Child: %.3f N/m, $\beta=0$' % w, linestyle='-', lw=3, zorder=1)
 plt.scatter(x_B0, y_B0, c='.3', label=r'Rao: %.3f N/m, $\beta=0$' %
             w, edgecolors='k', zorder=2, marker='s')
 
-# print('CASE 2')
-# l = loads(distributed_load=distributed_load, follower=True)
-# b = beam_chen(g, p, l, s, ignore_ends=True)
-# b.parameterized_solver(format_input=format_input, x0=g.D[:-1])
-# plt.plot(b.x, b.y, '.5', label=r'Child: %.3f N/m, $\beta=1$' % w, linestyle='-', lw=3, zorder=1)
-# plt.scatter(x_B1, y_B1, c='.5', label=r'Rao: %.3f N/m, $\beta=1$' %
-#             w, edgecolors='k', zorder=2, marker='s')
+print('CASE 2')
+l = loads(distributed_load=distributed_load, follower=True)
+b = beam_chen(g, p, l, s, ignore_ends=True)
+b.parameterized_solver(format_input=format_input, x0=g.D[:-1])
+plt.plot(b.x, b.y, '.5', label=r'Child: %.3f N/m, $\beta=1$' % w, linestyle='-', lw=3, zorder=1)
+plt.scatter(x_B1, y_B1, c='.5', label=r'Rao: %.3f N/m, $\beta=1$' %
+            w, edgecolors='k', zorder=2, marker='s')
 plt.xlim([0, 1])
 # plt.scatter(abaqus_data['coord'][0:401:40,0], abaqus_data['coord'][0:401:40,1], c='g', label='FEA', edgecolors='k', zorder = 10)
 plt.legend()